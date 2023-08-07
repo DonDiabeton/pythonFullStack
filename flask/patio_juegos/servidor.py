960b2c5a3cbf3385d15ff33f8c0c06446abd202a
DonDiabeton <andres.mingo.pavez@alumnos.sip.cl>
2023-08-07 14:28:57 -0400

    pausa

--- flask/patio_juegos/servidor.py
+++ flask/patio_juegos/servidor.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/play')                           
+def play():
+    return render_template('index.html')
+
+@app.route('/play/<int:num>/<string:color>')                           
+def repeat_square(num, color):
+    return render_template('index.html')
+
 
 if __name__=="__main__":
     app.run(debug=True)                   
