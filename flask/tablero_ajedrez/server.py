cab81a6e9eeac21158170c5f53889eccf03336e7
DonDiabeton <andres.mingo.pavez@alumnos.sip.cl>
2023-08-09 13:00:14 -0400

    hola

--- flask/tablero_ajedrez/server.py
+++ flask/tablero_ajedrez/server.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
 
+@app.route('/<int:x>/<int:y>')
+def chessboard(x, y):
 
+  return render_template('index.html', rows=x, cols=y)
   
 if __name__ == '__main__':
   app.run(debug=True)
